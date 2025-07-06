e2a242e462056262b7dcb36f8d44d8a85b29190f
Muzammil Shah <101082368+Muzammil803@users.noreply.github.com>
2025-07-07 02:53:34 +0500

    Update main.tsx

--- react-word-spell-checker/src/main.tsx
+++ react-word-spell-checker/src/main.tsx
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import App from "./App";
 export { default as SpellCheckerWrapper } from "./components/SpellCheckerWrapper";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
